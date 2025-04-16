fef93ce7213aa243592be692b25262fef1d782ae
Adam Overa <adamovera@protonmail.com>
2025-04-16 14:23:00 -0400

    Rename extension to Nested, update logo and functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "indentlines" is now active!');
+    // Command to add 4 spaces to the beginning of selected lines
+    let addSpacesCommand = vscode.commands.registerCommand('nested.addIndent', () => {
+        const editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('indentlines.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from IndentLines!');
-	});
+        if (editor) {
+            const selection = editor.selection;
+            const selectedText = editor.document.getText(selection);
 
-	context.subscriptions.push(disposable);
+            // Add 4 spaces to the beginning of each line in the selection
+            const indentedText = selectedText.split('\n').map(line => '    ' + line).join('\n');
+
+            // Replace the selected text with the indented version
+            editor.edit(editBuilder => {
+                editBuilder.replace(selection, indentedText);
+            });
+        }
+    });
+
+    // Command to remove 4 spaces from the beginning of selected lines
+    let removeSpacesCommand = vscode.commands.registerCommand('nested.removeIndent', () => {
+        const editor = vscode.window.activeTextEditor;
+
+        if (editor) {
+            const selection = editor.selection;
+            const selectedText = editor.document.getText(selection);
+
+            // Remove 4 spaces from the beginning of each line in the selection
+            const unindentedText = selectedText.split('\n').map(line => line.startsWith('    ') ? line.slice(4) : line).join('\n');
+
+            // Replace the selected text with the unindented version
+            editor.edit(editBuilder => {
+                editBuilder.replace(selection, unindentedText);
+            });
+        }
+    });
+
+    context.subscriptions.push(addSpacesCommand, removeSpacesCommand);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
